5249f58d62ef4a63d148dd536bb0fd35fab17514
wdalmeida <wazaa8@gmail.com>
2016-03-01 18:58:17 +0100

    Initial commit

---
+++ src/pds_banque/ClasseDeTest.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pds_banque;
+
+import pds_banque.ControleurEcranCreationCompte;
+/**
+ *
+ * @author flesguer
+ */
+public class ClasseDeTest {
+      public static void main(String[] args)
+  {
+    new ControleurEcranCreationCompte ("TestNom","TestPrenom","TestTelephone","TestMDP", "0", "1", "1");
+    
+  }
+ }
---
+++ src/pds_banque/Pds_banque.java
@@ -0,0 +1,9 @@
+package pds_banque;
+
+public class Pds_banque
+{
+  public static void main(String[] args)
+  {
+    EcranConnexion.main(args);
+  }
+}
