f5be252398d2ecac9c191216e48f40f07acd0d27
Florian <nomadkiss@gmail.com>
2016-03-30 02:31:03 +0200

    Phase de test d'une requête SQL de bout en bout: le serveur peut mantenant décoder le JSon envoyé par le client.

---
+++ src/pds_banque/Server/TCPServerDBJson.java
@@ -0,0 +1,97 @@
+package pds_banque.Server;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import org.json.simple.parser.ParseException;
+import java.io.IOException;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+
+/**
+ *
+ * @author Florian
+ */
+public class TCPServerDBJson {
+
+    public static void lancerServeur(int port) throws IOException, FileNotFoundException, ParseException {
+        ServerSocket socketAccueil = new ServerSocket(port);
+        String donneesEntreeClient;
+
+        while (true) {
+            Socket connectionSocket = socketAccueil.accept();
+            BufferedReader entreeVenantDuClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+            DataOutputStream sortieVersClient = new DataOutputStream(connectionSocket.getOutputStream());
+            donneesEntreeClient = entreeVenantDuClient.readLine();
+            System.out.println("Donnees recues par le serveur: " + donneesEntreeClient);
+
+            //a partir de ce moment le but est de transformer la chaine de caractere lue par le serveur en un objet
+            //il faudrait que cet objet soit un JsonObject dans l ideal
+            Object objetjson = donneesEntreeClient;
+            
+            //bug a corriger
+            JSONParser parser = new JSONParser();
+        Object obj;
+        obj = parser.parse(donneesEntreeClient);
+        decodageCustomer(obj);
+        //decodageCustomer(objetjson);
+
+            //AccessDB_server.insertionCustomer(donneesEntreeClient);
+           // sortieVersClient.writeBytes("Requete effectuee");
+
+        }
+
+    }
+
+    public static void decodageCustomer(Object objetjson) throws FileNotFoundException, IOException, ParseException {
+        
+        String Newligne=System.getProperty("line.separator"); 
+        System.out.print(objetjson + Newligne);
+        System.out.print("Recu cinq sur cinq");
+        
+        JSONObject jsonObject = (JSONObject) objetjson;
+        long id_Customer = (long) jsonObject.get("id_Customer");
+        String title_Customer = (String) jsonObject.get("title_Customer");
+        String last_Name_Customer = (String) jsonObject.get("last_Name_Customer");
+        String first_Name_Customer = (String) jsonObject.get("first_Name_Customer");
+        double salary_Customer = (double) jsonObject.get("salary_Customer");
+        String street_Customer = (String) jsonObject.get("street_Customer");
+        String pc_Customer = (String) jsonObject.get("pc_Customer");
+        String city_Customer = (String) jsonObject.get("city_Customer");
+        String phone_Customer = (String) jsonObject.get("phone_Customer");
+        String email_Customer = (String) jsonObject.get("email_Customer");
+        String birthday_Customer = (String) jsonObject.get("birthday_Customer");
+        long owner_Customer = (long) jsonObject.get("owner_Customer");
+        long id_Consultant = (long) jsonObject.get("id_Consultant");
+        long id_User = (long) jsonObject.get("id_User");
+        long id_status = (long) jsonObject.get("id_status");
+        System.out.println("id_Customer: " + id_Customer);
+        System.out.println("title_Customer: " + title_Customer);
+        System.out.println("last_Name_Customer: " + last_Name_Customer);
+        System.out.println("first_Name_Customer: " + first_Name_Customer);
+        System.out.println("salary_Customer: " + salary_Customer);
+        System.out.println("street_Customer: " + street_Customer);
+        System.out.println("pc_Customer: " + pc_Customer);
+        System.out.println("city_Customer: " + city_Customer);
+        System.out.println("phone_Customer: " + phone_Customer);
+        System.out.println("email_Customer: " + email_Customer);
+        System.out.println("birthday_Customer: " + birthday_Customer);
+        System.out.println("owner_Customer: " + owner_Customer);
+        System.out.println("id_Consultant: " + id_Consultant);
+        System.out.println("id_User: " + id_User);
+        System.out.println("id_status: " + id_status);
+                
+    }
+
+    public static void main(String argv[]) throws Exception {
+
+        lancerServeur(3000);
+
+    }
+}
--- src/pds_banque/Server/TestInsertion.java
+++ src/pds_banque/Server/TestInsertion.java
@@ -5,23 +5,106 @@
  */
 package pds_banque.Server;
 
+import java.io.DataOutputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import static pds_banque.Server.encodageJsonServer.encodage;
+import java.net.Socket;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.ParseException;
 
 /**
  *
  * @author Florian
  */
 public class TestInsertion {
- 
+
     public static void main(String[] args) throws IOException {
 
+        //je balance dans l'encodeur Json
+        //l'encodeur Json balance dans le client
+        //le client balance l'objet Json dans le serveur java
+        //le serveur Java balance dans le decodeur Json
+        //le decodeur Json balance dans la classe d'accès à la base de données
         float salaire = (float) 4500;
 
-        encodage(4, "", "Mickael", "Json", salaire, "18 Rue de Paris", "94225", "Charenton le Pont", "0125476936", "mickael.jackson@gmail.com",
-                "1958-08-29", 1, 2, 0, 1);
+        encodage(1, "", "Mickael", "Json", salaire, "18 Rue de Paris", "94225", "Charenton le Pont", "0125476936", "mickael.jackson@gmail.com",
+                "1958-08-29", 1, 1, 0, 1);
 
     }
-    
-    
+
+    public static void encodage(int id_Customer, String title_Customer, String last_Name_Customer, String first_Name_Customer, Float salary_Customer, String street_Customer, String pc_Customer, String city_Customer, String phone_Customer, String email_Customer, String birthday_Customer, int owner_Customer, int id_Consultant, int id_User, int id_status) throws IOException {
+
+        JSONObject obj = new JSONObject();
+
+        obj.put("id_Customer", id_Customer);
+        obj.put("title_Customer", title_Customer);
+        obj.put("last_Name_Customer", last_Name_Customer);
+        obj.put("first_Name_Customer", first_Name_Customer);
+        obj.put("salary_Customer", salary_Customer); //! float
+        obj.put("street_Customer", street_Customer);
+        obj.put("pc_Customer", pc_Customer);
+        obj.put("city_Customer", city_Customer);
+        obj.put("phone_Customer", phone_Customer);
+        obj.put("email_Customer", email_Customer);
+        obj.put("birthday_Customer", birthday_Customer); //! date
+        obj.put("owner_Customer", owner_Customer);
+        obj.put("id_Consultant", id_Consultant);
+        obj.put("id_User", id_User);
+        obj.put("id_status", id_status);
+
+        System.out.print("Objet encodé:" + obj);
+        System.out.println("1");
+        RequeteTCPJson(obj);
+        System.out.println("2");
+    }
+
+    public static void RequeteTCPJson(JSONObject objetJson) throws IOException {
+        String host = "localhost";
+        int port = 3000;
+        String jsonString = objetJson.toString();
+        try (
+            Socket socketClient = new Socket(host, port)) {
+            System.out.println("3");
+            DataOutputStream sortieVersServeur = new DataOutputStream(socketClient.getOutputStream());
+            sortieVersServeur.writeBytes(jsonString + '\n');
+            socketClient.close();
+        }
+    }
+
+    public static void decodageCustomer(JSONObject objetjson) throws FileNotFoundException, IOException, ParseException {
+
+        
+        long id_Customer = (long) objetjson.get("id_Customer");
+        String title_Customer = (String) objetjson.get("title_Customer");
+        String last_Name_Customer = (String) objetjson.get("last_Name_Customer");
+        String first_Name_Customer = (String) objetjson.get("first_Name_Customer");
+        double salary_Customer = (double) objetjson.get("salary_Customer");
+        String street_Customer = (String) objetjson.get("street_Customer");
+        String pc_Customer = (String) objetjson.get("pc_Customer");
+        String city_Customer = (String) objetjson.get("city_Customer");
+        String phone_Customer = (String) objetjson.get("phone_Customer");
+        String email_Customer = (String) objetjson.get("email_Customer");
+        String birthday_Customer = (String) objetjson.get("birthday_Customer");
+        long owner_Customer = (long) objetjson.get("owner_Customer");
+        long id_Consultant = (long) objetjson.get("id_Consultant");
+        long id_User = (long) objetjson.get("id_User");
+        long id_status = (long) objetjson.get("id_status");
+        System.out.println("id_Customer: " + id_Customer);
+        System.out.println("title_Customer: " + title_Customer);
+        System.out.println("last_Name_Customer: " + last_Name_Customer);
+        System.out.println("first_Name_Customer: " + first_Name_Customer);
+        System.out.println("salary_Customer: " + salary_Customer);
+        System.out.println("street_Customer: " + street_Customer);
+        System.out.println("pc_Customer: " + pc_Customer);
+        System.out.println("city_Customer: " + city_Customer);
+        System.out.println("phone_Customer: " + phone_Customer);
+        System.out.println("email_Customer: " + email_Customer);
+        System.out.println("birthday_Customer: " + birthday_Customer);
+        System.out.println("owner_Customer: " + owner_Customer);
+        System.out.println("id_Consultant: " + id_Consultant);
+        System.out.println("id_User: " + id_User);
+        System.out.println("id_status: " + id_status);
+
+    }
+
 }
