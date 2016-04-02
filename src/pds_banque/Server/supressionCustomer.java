d6b130de1350f330bb24d2fa81e48ccb71395f14
Florian <nomadkiss@gmail.com>
2016-04-02 04:11:10 +0200

    Methodes encapsulees

--- src/pds_banque/Server/supressionCustomer.java
+++ src/pds_banque/Server/supressionCustomer.java
@@ -16,8 +16,6 @@ public class supressionCustomer {
         String supressionCustomer = "DELETE FROM customer WHERE last_Name_Customer='Mickael';";
         String supressionUser = "DELETE FROM user WHERE login_User LIKE 'jmickael';";
         System.out.println("Soit la requete SQL:" + '\n');
-        System.out.println(supressionCustomer);
-        System.out.println(supressionUser);
         AccessDB_server.envoyerRequeteUpdate(supressionCustomer);
         AccessDB_server.envoyerRequeteUpdate(supressionUser);
 
