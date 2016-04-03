d8287a27fdbdb2436f38bc74f4a5ffeb179fe96e
wdalmeida <wazaa8@gmail.com>
2016-04-03 20:23:52 +0200

    Modification on test file

--- test/pds_banque/Model/AccessDBTest.java
+++ test/pds_banque/Model/AccessDBTest.java
@@ -34,6 +34,7 @@ public class AccessDBTest {
 
     /**
      * Test of getConnexion method, of class AccessDB.
+     * @throws java.security.NoSuchAlgorithmException
      */
     @Test
     public void testGetConnexion() throws NoSuchAlgorithmException {
@@ -59,12 +60,12 @@ public class AccessDBTest {
      * Test of insertCustomer method, of class AccessDB.
      */
     @Test
-    public void testInsertCustomer() {
+    public void testInsertCustomer() throws NoSuchAlgorithmException {
         System.out.println("insertCustomer");
         Customer cust = null;
         AccessDB instance = null;
         int expResult = 0;
-        int result = instance.insertCustomer(cust);
+        int result = instance.insertCustomer(cust,1);
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
