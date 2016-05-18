0e68315b03660502b0f5afda4f557b83b19a971a
polycarpeflorent <polycarpeflorent@yahoo.fr>
2016-05-18 16:53:33 +0200

    change file Controller and ScreenCompare, SimulationTable

---
+++ src/edu/god/controllers/ControllerScreenCompareSimulation.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.god.controllers;
+
+import edu.god.views.ScreenCompareSimulation;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JButton;
+
+/**
+ *
+ * @author florent
+ */
+public class ControllerScreenCompareSimulation implements ActionListener {
+
+    private JButton btnCompareSimulation;
+    private JButton btnClose;
+    private ScreenCompareSimulation screenCompareSimulation;
+    private int idCustomer;
+
+    /**
+     * @param idCustomer0
+     * @param btnCompareSimulation0
+     */
+    public ControllerScreenCompareSimulation(int idCustomer0, JButton btnCompareSimulation0) {
+        this.btnCompareSimulation = btnCompareSimulation0;
+        this.idCustomer = idCustomer0;
+    }
+
+    public ControllerScreenCompareSimulation(ScreenCompareSimulation screenCompareSimulation0, JButton btnClose0) {
+        this.btnCompareSimulation = btnClose0;
+        this.screenCompareSimulation = screenCompareSimulation0;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == btnCompareSimulation) {
+            try {
+                System.out.println("bouton Compare Simulation");
+                ScreenCompareSimulation scs = new ScreenCompareSimulation(idCustomer);
+            } catch (SQLException ex) {
+                Logger.getLogger(ControllerScreenHome.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } else if (e.getSource() == btnClose) {
+            System.out.println("bouton Close Compare Simulation");
+            screenCompareSimulation.dispose();
+            screenCompareSimulation.setVisible(false);
+        }
+    }
+}
